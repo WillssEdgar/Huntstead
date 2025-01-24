bc6575cfa093262b77ac7f323b4852795f52f2f7
WillssEdgar <willssedgar2123@gmail.com>
2025-01-24 15:01:01 -0500

    feat: creating database and endpoint on backend

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -15,6 +18,21 @@ func main()  {
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
     w.Write([]byte("Hello, World!"))
   })
+
+	r.Post("/sign-up", func(w http.ResponseWriter, r *http.Request) {
+		var data map[string]interface{}
+		if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+			http.Error(w, "Invalid JSON", http.StatusBadRequest)
+			return
+		}
+
+		// Print the data to the console
+		log.Printf("Received data: %+v\n", data)
+
+		// Send a simple response back
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintln(w, "Data received and logged.")
+	})
 
 	http.ListenAndServe(":3000",r);
 }
